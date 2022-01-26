3128f0c3da7a324bc7874fa37acb2e276b8b9f73
udaya <udayeswari.savana@gmail.com>
2022-01-25 21:28:04 -0500

    copy nginx roboshop config start nginx service

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -34,7 +34,7 @@ echo "copy the extracted content to Nginx Path"
 cp -r frontend-main/static/* /usr/share/nginx/html/ &>>$LOG_FILE
 
 echo "copy Nginx Roboshop Config"
-cp frontend.main/localhost.conf/etc/nginx/html/&>>LOG_FILE
+cp frontend-main/localhost.conf/etc/nginx/html/ &>>LOG_FILE
 
 echo "Start Nginx Service"
 systemctl enable nginx &>>$LOG_FILE
