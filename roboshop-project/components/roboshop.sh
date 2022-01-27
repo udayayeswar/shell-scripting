b40f69d45518e5faba622f322691a2bd41a07aa3
udaya <udayeswari.savana@gmail.com>
2022-01-27 07:39:04 -0500

    more code has been added

--- roboshop-project/components/roboshop.sh
+++ roboshop-project/components/roboshop.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-bash components/$1.sh
+if [ -f components/$1.sh ]; then
+  bash components/$1.sh
+else
+  echo -e "\e[1;31mInvalid Input\e[0m"
+  echo -e "\e[1;33mAvalible Inputs - frontend|mongodb|catalogue|redis|users|cart|dispatch|mysql|payment|rabbitmq|shipping\e[0m"
