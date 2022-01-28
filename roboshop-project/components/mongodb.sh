eb1be60e4594f2c7c13d4eaaaeae40f929df1e4a
udaya <udayeswari.savana@gmail.com>
2022-01-27 21:59:35 -0500

    command has been added

--- roboshop-project/components/mongodb.sh
+++ roboshop-project/components/mongodb.sh
@@ -30,33 +30,33 @@
 #Symbol `<` will take the input from a file and give that input to the command.
 echo mongodb
 
-LOG_FILE=/tmp/roboshop.log
-rm -f $LOG_FILE
+#LOG_FILE=/tmp/roboshop.log
+#rm -f $LOG_FILE
 
 echo "Download Mongodb repo File"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo  &>>$LOG_FILE
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
 
-echo "Install MongoDB"
-yum install -y mongodb-org &>>$LOG_FILE
-
-echo "Upate MongoDB config file"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
-
-echo "Start Database"
-systemctl enable mongod &>>$LOG_FILE
-systemctl start mongod &>>$LOG_FILE
-
-echo "Download Schema"
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-
-echo "Extract Schema"
-cd /tmp/
-unzip -o mongodb.zip &>>$LOG_FILE
-
-echo "Load schema"
-cd mongodb-main
-mongo < catalogue.js &>>$LOG_FILE
-mongo < users.js &>>$LOG_FILE
+#echo "Install MongoDB"
+#yum install -y mongodb-org
+#
+#echo "Upate MongoDB config file"
+#sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+#
+#echo "Start Database"
+#systemctl enable mongod
+#systemctl start mongod
+#
+#echo "Download Schema"
+#curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+#
+#echo "Extract Schema"
+#cd /tmp/
+#unzip -o mongodb.zip &>>$LOG_FILE
+#
+#echo "Load schema"
+#cd mongodb-main
+#mongo < catalogue.js &>>$LOG_FILE
+#mongo < users.js &>>$LOG_FILE
 
 
 
