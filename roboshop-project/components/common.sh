1f7b9f869e90f633772cff42ac7d30af95f1b794
udaya <udayeswari.savana@gmail.com>
2022-01-29 18:34:00 -0500

    command has been added

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -1,2 +1,10 @@
 LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
+
+
+if [$1 -eq 0 ]; then
+  echo -e "\e[1;32m SUCCESS\e[0m"
+  else
+    echo -e "\e[1;31m FAILED\e[0m"
+  exit
+ fi
--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -14,31 +14,38 @@
 
 # systemctl restart nginx
 echo "frontend"
-source components/common.sh
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
 
 echo "installing NGINX"
 yum install nginx -y  &>>$LOG_FILE
+STAT $?
 
 echo "Download Frontend Content"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"  &>>$LOG_FILE
+STAT $?
 
 echo "clean old content"
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+STAT $?
 
 echo "Extract frontend Content"
 cd /tmp
 unzip -o frontend.zip &>>$LOG_FILE
+STAT $?
 
 echo "copy Extracted content to Nginx path"
 cp -r frontend-main/static/* /usr/share/nginx/html/ &>>$LOG_FILE
+STAT $?
 
 echo "Copy Nginx roboshop config"
 cp frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+STAT $?
 
 echo "Start Nginx Service"
 systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx  &>>$LOG_FILE
-
+STAT $?
 
 
 
