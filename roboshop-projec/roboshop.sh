5ab9db3785d304eb69f5ca1f2c1d907ed81a18c1
udaya <udayeswari.savana@gmail.com>
2022-01-30 18:27:35 -0500

    command has been added

--- roboshop-projec/roboshop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -f components/$1.sh ] ; then
-  bash components/$1.sh
-else
-  echo -e "\e[1;31mInvalid Input\e[0m"
-  echo -e "\e[1;33mAvalible Inputs - frontend|mongodb|catalogue|redis|users|cart|dispatch|mysql|payment|rabbitmq|shipping\e[0m"
- fi
