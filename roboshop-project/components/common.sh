8ab573a2222796da1192f67b81b000f1953204b4
udaya <udayeswari.savana@gmail.com>
2022-01-29 21:45:17 -0500

    command has been added

--- roboshop-project/components/common.sh
+++ roboshop-project/components/common.sh
@@ -3,10 +3,11 @@
 LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 rm -f /etc/yum.repos.d/endpoint.repo
-
+STAT() {
 if [$1 -eq 0 ]; then
   echo -e "\e[1;32m SUCCESS\e[0m"
   else
     echo -e "\e[1;31m FAILED\e[0m"
-  exit
+  exit 2
  fi
+}
