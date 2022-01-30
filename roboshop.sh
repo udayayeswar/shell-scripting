162005be7143ea07b234ff3861a2818597a70be5
udaya <udayeswari.savana@gmail.com>
2022-01-30 18:34:14 -0500

    command has been added

--- roboshop.sh
+++ roboshop.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+  echo -e "\e[1;31m You should be root user to execute this script ..\e[0m"
+  exit 1
+fi
+
 if [ -f components/$1.sh ] ; then
   bash components/$1.sh
 else
