ed1c7ad4a06567333b19a3e4b6884504d0fed0d1
udaya <udayeswari.savana@gmail.com>
2022-01-30 19:22:58 -0500

    command has been added

--- roboshop-project/components/mongodb.sh
+++ roboshop-project/components/mongodb.sh
@@ -31,35 +31,39 @@
 #Symbol `<` will take the input from a file and give that input to the command.
 echo mongodb
 
-LOG_FILE=/tmp/roboshop.log
-rm -f $LOG_FILE
 
-rm -f /etc/yum.repos.d/endpoint.repo
+source components/common.sh
 
 echo "Download Mongodb repo File"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
+STAT $?
 
 echo "Install MongoDB"
 yum install -y mongodb-org &>>$LOG_FILE
+STAT $?
 
 echo "Upate MongoDB config file"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
-
+STAT $?
 echo "Start Database"
 systemctl enable mongod &>>$LOG_FILE
+STAT $?
 systemctl start mongod &>>$LOG_FILE
-
+STAT $?
 echo "Download Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
-
+STAT $?
 echo "Extract Schema"
 cd /tmp/
 unzip -o mongodb.zip &>>$LOG_FILE
-
+STAT $?
 echo "Load schema"
 cd mongodb-main
-mongo < catalogue.js &>>$LOG_FILE
-mongo < users.js &>>$LOG_FILE
+
+for app in catalogue users ; do
+  mongo < ${app}.js &>>$LOG_FILE
+done
+STAT $?
 
 
 
