91c6a05b5158cd0186a30d677e3be6d6b634e5a0
udaya <udayeswari.savana@gmail.com>
2022-01-31 12:03:19 -0500

    command has been added

--- roboshop-project/components/create-ec2.sh
+++ roboshop-project/components/create-ec2.sh
@@ -23,7 +23,7 @@ INSTANCE_CREATE() {
 
   if [ -z "${PRIVATE_IP}" ]; then
     # Find Security Group
-    SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=allow-all-ports --query "SecurityGroups[*].GroupId" --output text)
+    SG_ID=$(aws ec2 describe-security-groups --filter Name=group-name,Values=allow-all --query "SecurityGroups[*].GroupId" --output text)
     if [ -z "${SG_ID}" ]; then
       echo -e "\e[1;33m Security Group allow-all-ports does not exist"
       exit
