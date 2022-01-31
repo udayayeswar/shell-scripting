7d873bd4448caccd0fdb6984043d4ac44f866af4
udaya <udayeswari.savana@gmail.com>
2022-01-31 16:42:27 -0500

    command has been added

--- roboshop-project/components/catalogue.sh
+++ roboshop-project/components/catalogue.sh
@@ -42,50 +42,6 @@ echo catalogue
 # systemctl enable catalogue
 #
 #```
-LOG_FILE=/tmp/roboshop.log
-rm -f $LOG_FILE
+source components/common.sh
 
-
-echo "setup Nodejs repo"
- curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>$LOG_FILE
-
-echo "Install Nodejs"
-yum install nodejs gcc-c++ -y &>>$LOG_FILE
-
-echo "create app user"
-useradd roboshop &>>$LOG_FILE
-
-echo "Download catalogue code"
-curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
-
-echo "Extract Catalogue code"
-cd /tmp/
-unzip -o catalogue.zip &>>$LOG_FILE
-
-echo "Clean Old Catalogue"
-rm -rf /home/roboshop/catalogue
-
-echo "copy Catalogue content"
-cp -r catalogue-main /home/roboshop/catalogue
-
-echo "Install NodeJS dependencies"
-cd /home/roboshop/catalogue
-npm install &>>$LOG_FILE
-
-chown roboshop:roboshop /home/roboshop/ -R &>>$LOG_FILE
-
-echo "Updata SystemD file"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop-internal/'
- /home/roboshop/catalogue/systemd.service &>>$LOG_FILE
-
-echo "Setup Catalogue SystemD file"
-mv  /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
-
-echo "Start Catalogue"
-systemctl daemon-reload &>>$LOG_FILE
-systemctl enable catalogue &>>$LOG_FILE
-systemctl start catalogue &>>$LOG_FILE
-
-
-
-
+NODEJS catalogue
